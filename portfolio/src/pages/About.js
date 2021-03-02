8045f54383365036fbf9c3c06b7547743f5b7e1c
Rand Hale <randluca@gmail.com>
2021-03-02 10:09:33 -0800

    fix resume link

--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import resume from "../files/Rand_Hale_Resume.pdf";
 
 const styles = {
     mainbox: {
@@ -20,7 +20,7 @@ function About() {
                 <h2 style={{ fontWeight: "bold" }}>About Me</h2>
                 <hr />
                 <p>My name is Rand Hale and I am a junior full stack web developer. I have been primarily exposed to the MERN stack and I am working daily to refine my expertise with its associated technologies. I am currently based in the San Francisco Bay Area, but I hope to move to Los Angeles within the year.</p>
-                <p>A PDF download of my resume is available <Link to="/files/Rand_Hale_Resume.pdf" download target="_blank" style={styles.a}>here</Link>.</p>
+                <p>A PDF download of my resume is available <a href={resume} rel="noreferrer" target="_blank" style={styles.a}>here</a>.</p>
                 <div className="row justify-content-center">
                     <p className="col-md-3">Contact: <a style={styles.a} target="_blank" rel="noreferrer" href="mailto:randluca@gmail.com">randluca@gmail.com</a></p>
                     <p className="col-md-3">LinkedIn: <a style={styles.a} target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/rand-hale-83ba389b/">Rand Hale</a></p>
