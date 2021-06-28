32aba4445fca511057abf69a5fc4b38856f77f1c
Rand Hale <randluca@gmail.com>
2021-06-28 10:05:25 -0700

    use React HashRouter instead of BrowserRouter for gh-pages compatibility

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -11,7 +11,7 @@ function App() {
   return (
     <div id="wrapper">
       <div id="content-wrap">
-        <Router >
+        <Router>
           <Header />
           <Switch>
           <Route exact path="/" component={Home} />
